d131c78965e179f0428b9b60b74939b32686a644
RahulDittakavi <142203611+RahulDittakavi@users.noreply.github.com>
2025-09-22 20:53:17 +0530

    commit#1
    GRPC communication is up and running. Still haven't integrated Kafka

---
+++ billing-service/src/test/java/com/pm/patientbillingservice/PatientBillingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pm.patientbillingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientBillingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
